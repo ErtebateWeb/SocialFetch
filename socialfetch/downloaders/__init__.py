1812c081cac8a9bd3f30678d7c60309dfaad9f2d
Architect Agent <architect@socialfetch.dev>
2026-07-16 16:38:07 +0000

    fix: register youtube downloader in __init__.py

--- socialfetch/downloaders/__init__.py
+++ socialfetch/downloaders/__init__.py
@@ -2,6 +2,7 @@
 
 # Import all downloader modules to trigger registry registration
 from socialfetch.downloaders import instagram  # noqa: F401
+from socialfetch.downloaders import youtube  # noqa: F401
 from socialfetch.downloaders.registry import DownloaderRegistry
 
 __all__ = ["DownloaderRegistry"]
